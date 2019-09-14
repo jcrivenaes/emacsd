2d04e307968bcd88517b3ced1f08bf66450be3db
Jan C. Rivenaes <jan@rivenaes.net>
2019-09-14 12:02:39 +0200

    removed elpa

--- elpa/pylint-20170402.1255/pylint-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "pylint" "20170402.1255" "minor mode for running `pylint'" 'nil :commit "6b3afd4f6d75debd7f286f0d3c760ed10ab1e79f" :keywords '("languages" "python") :authors '(("Ian Eure" . "ian.eure@gmail.com")) :maintainer '("Jonathan Kotta" . "jpkotta@gmail.com"))
